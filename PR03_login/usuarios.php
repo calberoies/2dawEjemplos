fde2f6e83f1a48e87f195e94bbcd53b803c99e62
Cecilio <calbero@iesfuentesanluis.org>
2018-10-26 12:50:11 +0200

    m

---
+++ PR03_login/usuarios.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'admin'=>['password'=>'1234','nombre'=>'Administrador'],
+    'pepe'=> ['password'=>'1234','nombre'=>'Pepe García'],
+    'ana'=> ['password'=>'1234','nombre'=>'Ana Martínez'],
+];
